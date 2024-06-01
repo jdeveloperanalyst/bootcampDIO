29bc617eb3fe2c9593f7c1451926b16c467ab6e1
jdeveloperanalyst <jonatas1200@icloud.com>
2024-05-31 22:23:07 -0300

    hotfix:desafio 2 completo

--- Desafios/desafio_2.py
+++ Desafios/desafio_2.py
@@ -6,6 +6,10 @@ def menu_principal():
 [1] - Depositar
 [2] - Sacar
 [3] - Extrato
+[4] - Criar Usuário
+[5] - Criar Conta Corrente
+[6] - Consultar Usuário
+[7] - Listar Usuários
 [0] - Sair
 
 => '''
@@ -52,10 +56,83 @@ def extrato(saldo,/,*,extrato):
     return ''
 
 
+def criar_usuario(list_users):
+    nome = str(input('Nome completo => '))
+    data_nascimento = str(input('Data de Nascimento => '))
+    cpf = int(input('CPF (sem os pontos/traços) => '))
+    for user in list_users:
+        if cpf == user['cpf']:
+            print('O CPF já existe em nossa base de dados\n')
+            print(user)
+            return list_users
+    endereco = ' - '.join([str(input('Logradouro => ')), str(input('Número => ')), str(input('Bairro => ')), str(input('Cidade/Sigla Estado => '))])
+    
+    usuario = {
+        'nome': nome,
+        'data nascimento': data_nascimento,
+        'cpf': cpf,
+        'endereço': endereco 
+    }
+    list_users.append(usuario)
+    return list_users
+
+
+def criar_conta_corrente(list_users, contas):
+    if len(list_users) > 0:
+        cpf = int(input('Informe o cpf(sem pontos/traços) => '))
+        cpf_encontrado = False
+        for user in list_users:
+            if cpf == user['cpf']:
+                conta = ' | '.join(['agência: 0001', f'conta {len(contas)+1}'])
+                contas.append(conta)
+                if 'conta(s)' in user:
+                    user['conta(s)'].add(conta)
+                else:
+                    user['conta(s)'] = {conta}
+                cpf_encontrado = True
+        if not cpf_encontrado:
+            print('\nO CPF não existe em nossa base de dados')
+            return list_users, contas
+    else:
+        print('O banco ainda não possui usuários')
+    return list_users, contas
+        
+
+def consultar_usuario(list_users):
+    if len(list_users) > 0:
+        cpf = int(input('Informe o cpf(sem pontos/traços) => '))
+        cpf_encontrado = False
+        for user in list_users:
+            if cpf == user['cpf']:
+                print('=' * 60)
+                for chave, valor in user.items():
+                    print(f'{chave}: {valor}')
+                print('=' * 60)
+                cpf_encontrado = True
+        if not cpf_encontrado:
+            print('CPF não existe em nossa base de dados!')
+    else:
+        print('O banco ainda não possui usuários')
+        
+
+def listar_usuarios(list_users):
+    if len(list_users) > 0:
+        for user in list_users:
+            print('=' * 60)
+            for chave, valor in user.items():
+                print(f'{chave}: {valor}')
+            print('=' * 60)
+    else:
+        print('O banco ainda não possui usuários')
+        
+       
 if __name__ == '__main__':
     saldo = 0
     extrato_detalhado = []
     numero_saques = 1
+    lista_usuarios = [] 
+    contas = []
+       
     while True:
         opcao = int(input(menu_principal()))
         if opcao == 1:
@@ -66,6 +143,18 @@ if __name__ == '__main__':
             saldo, extrato_detalhado, numero_saques = sacar(saldo=saldo, valor=valor, extrato=extrato_detalhado, limite=500, numero_saques=numero_saques, limite_saques=3)
         elif opcao == 3:
             extrato(saldo, extrato=extrato_detalhado)
+        elif opcao == 4:
+            print('\nCriar Usuário\n')
+            lista_usuarios = criar_usuario(lista_usuarios)
+        elif opcao == 5:
+            print('\nCriar Conta Corrente\n')
+            lista_usuarios, contas = criar_conta_corrente(lista_usuarios, contas)
+        elif opcao == 6:
+            print('\nConsulta de Usuarios\n')
+            consultar_usuario(lista_usuarios)
+        elif opcao == 7:
+            print('\nLista de Usuários\n')
+            listar_usuarios(lista_usuarios)
         elif opcao == 0:
             print(f'\nMuito Obrigado por utilizar os nossos serviços.\nVolte Sempre :)\n')
             break
